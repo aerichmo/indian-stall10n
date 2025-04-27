001a30927ea83e256045b160b61673a08eba0905
aerichmo <alecrichmo@gmail.com>
2025-04-27 00:59:15 -0500

    Delete infra directory

--- infra/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "region"   { default = "us-central1" }
-variable "cron"     { default = "0 15 * * *" }  # 10 a.m. CT
-variable "api_user" { type = string }
-variable "api_pass" { type = string }
